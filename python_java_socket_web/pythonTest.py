98d7a65bf99dc8abc80b1943ec3d52ea1b7aaa27
wanggang1906 <1294901911@qq.com>
2020-05-30 18:58:43 +0800

    连接java测试

---
+++ python_java_socket_web/pythonTest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# 不引入第三方包的
+def connectJava(arges,param):
+    print(arges)
+    print(param)
+    print("ok")
+
+
+
+if __name__ == '__main__':
+    arges = "python"
+    param = "de"
+    connectJava(arges,param)
